e8a1d1531cb3b64f8c74a3eead01d75487cc90a8
Bharath Kumar <bharathkumarkarthick@gmail.com>
2025-08-14 10:38:57 +0530

    use vite meta for env

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 // fallback urls for leagues and badges
 // these can be overridden by environment variables
 // LEAGUE_URL and BADGE_URL
@@ -8,9 +5,9 @@ dotenv.config();
 
 export const settings = {
   leaguesUrl:
-    process.env.LEAGUE_URL ||
+    import.meta.env.LEAGUE_URL ||
     "https://www.thesportsdb.com/api/v1/json/3/all_leagues.php",
   badgeUrl:
-    process.env.LEAGUE_URL ||
+    import.meta.env.LEAGUE_URL ||
     "https://www.thesportsdb.com/api/v1/json/3/search_all_seasons.php",
 };
